b18bf4a18dce0f155e81a863feff64f6040619c1
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-03-24 05:27:06 +0330

    create base

---
+++ Text_Encryption.py
@@ -0,0 +1,15 @@
+while True:
+    inputText = input('enter your text :')
+    resulttext = ""
+    
+    orderinput = input('1 for Encryption\n2 for decoding\n3 for exit ')
+    
+    if orderinput == "1":
+        pass
+    elif orderinput == "2":
+        pass
+    elif orderinput == "3":
+        pass
+    else:
+        pass
+    
